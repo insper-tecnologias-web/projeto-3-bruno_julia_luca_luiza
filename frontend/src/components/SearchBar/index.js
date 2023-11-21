42f773545be176a85a5c8b3416eeaf07d7c6ab74
LuCaruso <lucac@al.insper.edu.br>
2023-11-21 19:23:49 -0300

    filmes.map

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -7,12 +7,14 @@ import Filme from "../Filme";
 export const SearchBar = () => {
 
     const [input, setInput] = useState("");
+    const [filmes, setFilmes] = useState([]);
 
     const pegaDados = (value) => {
         value.preventDefault();
         axios
         //.get("https://moviefy-backend.onrender.com/title/" + value);
         .get("http://127.0.0.1:8000/title/" + input)
+        .then((res) => setFilmes(res.data));
     }
 
 
@@ -30,17 +32,40 @@ export const SearchBar = () => {
             </form>
 
             <div className="mx-[150px] flex flex-row flex-wrap justify-center ">
-                {input.map((filme) => (
+                {filmes.map((filme) => (
                     <div className="mx-2">
                         <Filme key={`filme__${filme.id}`} id={filme.id} capa={filme.primaryImage != null? filme.primaryImage.url:"https://fastly.picsum.photos/id/250/800/1200.jpg?hmac=mLfkxoNEwjCn6yE7Y7c4ExK1GoWmo69QwYcxQ7Rns_E"} title={filme.titleText.text} curtir={1} info={filme.plot.plotText.plainText}>{filme.releaseYear.year} </Filme>
                     </div>
                 ))}
             </div>
 
-
-
-        </div>
-        
-        
+        </div>  
     );
 };
+
+// export const SearchBar = (entrada) => {
+//     return (
+//         <div>
+//             <form className="input-wrapper" method="get" onSubmit={pegaDados}>
+//                 <input 
+//                 type="text" 
+//                 placeholder="Procure o filme"  
+//                 defaltvalue={entrada}
+//                 onChange={(e) => setInput(e.target.value)}
+//                 />
+//                 <button type="submit"> 
+//                     <FaSearch id="search-icon"/>
+//                 </button>
+//             </form>
+
+//             <div className="mx-[150px] flex flex-row flex-wrap justify-center ">
+//                 {filmes.map((filme) => (
+//                     <div className="mx-2">
+//                         <Filme key={`filme__${filme.id}`} id={filme.id} capa={filme.primaryImage != null? filme.primaryImage.url:"https://fastly.picsum.photos/id/250/800/1200.jpg?hmac=mLfkxoNEwjCn6yE7Y7c4ExK1GoWmo69QwYcxQ7Rns_E"} title={filme.titleText.text} curtir={1} info={filme.plot.plotText.plainText}>{filme.releaseYear.year} </Filme>
+//                     </div>
+//                 ))}
+//             </div>
+
+//         </div>  
+//     );
+// };
